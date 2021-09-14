99288623ffffe693a8fd017b1bcd58f7976e9095
Preston Carlton <prescarlton@gmail.com>
2021-09-14 17:34:47 -0400

    redesigned project archive, added caching so i don't ddos github

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
-      <Navbar active='home'/>
+      <Navbar active='home' />
       <div className='lines absolute top-64'>
         <div className='flex my-2'>
           <div className='line h-5 w-8 bg-gray-100 rounded-r-xl mr-2'></div>
@@ -21,8 +21,8 @@ export default function Home() {
 
       <div className='px-6 my-16 md:m-0 flex flex-1 flex-col md:px-36 justify-end items-end md:items-start md:justify-center'>
         <h1 className='text-4xl my-4'>Hey, I'm Preston.</h1>
-        <h3 className='text-gray-600 md:text-2xl md:w-3/5 text-right md:text-left my-1'>I'm a junior software developer from Charleston, SC. click below to check out some of the stuff i've been working on in my free time.</h3>
-        <button className='cursor-pointer bg-blue-500 py-2 px-6 rounded-md text-white hover:bg-blue-600 transition my-1'><Link href='/projects'>PROJECTS</Link></button>
+        <h3 className='text-gray-600 md:text-2xl md:w-3/5 text-right md:text-left my-1'>I'm a junior software developer from Charleston, SC. Click below to check out some of the stuff i've been working on in my free time.</h3>
+        <Link href='/projects'><button className='cursor-pointer bg-blue-500 py-2 px-6 rounded-md text-white hover:bg-blue-600 transition my-1'>PROJECTS</button></Link>
       </div>
       <div className='hidden md:absolute md:bottom-32 md:right-0 md:flex md:flex-col md:items-end'>
         <div className='flex my-2'>
--- pages/project-archive.js
+++ pages/project-archive.js
@@ -3,13 +3,52 @@ import { Navbar } from "../components/navbar";
 import { RepoCard } from "../components/RepoCard";
 import Link from 'next/link'
 import { ArrowLeft } from "react-feather";
+import { useEffect, useState } from "react";
 
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+const repoFetcher = (...args) => fetch(...args).then(res => sortRepos(res.json()));
+
+const sortRepos = (resp) => {
+    let repos = resp;
+    repos.sort((a, b) => Date.parse(b.updated_at) - Date.parse(a.updated_at))
+    // save to session storage
+    sessionStorage.setItem('prescarlton_repos', JSON.stringify(repos))
+    return repos;
+}
 
 export default function ProjectArchive() {
-    const { data, error } = useSWR('https://api.github.com/users/prescarlton/repos', fetcher);
-    if (!data) return <h1>Loading...</h1>;
-    console.log(data)
+    // check sessionStorage
+    // let data;
+    // if (!(sessionStorage?.getItem('prescarlton_repos'))) {
+    //     const resp = useSWR('https://api.github.com/users/prescarlton/repos', repoFetcher);
+    //     data = resp.data;
+    // } else {
+    //     data = JSON.parse(sessionStorage.getItem('prescarlton_repos'))
+    // }
+
+    const [repoList, setRepoList] = useState(null);
+    const [isInitialized, setIsInitialized] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+
+        // check session storage first
+        if (sessionStorage.getItem('prescarlton_repos'))
+            setRepoList(JSON.parse(sessionStorage.getItem('prescarlton_repos')))
+        else
+            // fetch repo list
+            fetch('https://api.github.com/users/prescarlton/repos')
+                .then(res => {
+                    res.json().then(data => {
+                        // if error, setErrorMsg
+                        if (data.message) {
+                            setErrorMsg(true)
+                            console.log(data)
+                        } else {
+                            setRepoList(sortRepos(data));
+                        }
+                    })
+                })
+    }, [])
     return (
         <div className='h-screen flex flex-col'>
             <Navbar active='projects' />
@@ -18,11 +57,20 @@ export default function ProjectArchive() {
                     <h1 className='text-4xl mb-1 font-black'>project archive</h1>
                     <div className='header-underline h-2 w-10 rounded-lg bg-blue-500'></div>
                 </div>
-                {data.message ? (<h2>Hey! You hit the GitHub API limit! Chill out before they send people after me</h2>) : data.map(repo => {
+                {(!repoList && !errorMsg) && (<h1>Loading...</h1>)}
+                {repoList && repoList.map(repo => {
                     return (
                         <RepoCard {...repo} key={repo.id} />
                     )
                 })}
+
+                {errorMsg && (
+                    <div>
+                        <h1 className='text-3xl'>Uhh, well this is awkward.</h1>
+                        <h2>I'm currently using GitHub's free API so I don't have to worry about authentication. The downside to that is that you're limited to 60 requests / hour per IP. I'm flattered that you're that interested but maybe calm down just a little bit?</h2>
+                    </div>
+
+                )}
             </div>
         </div>
     )
